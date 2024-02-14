7ea76423f0861efdef924c1f19d06a2fc87cfbef
dennishvf <dennisk38@nycstudents.net>
2024-02-14 09:23:50 -0500

    cmnvjhv

---
+++ vue-project/src/components/DestCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+<h1>{{ Destination.name }}</h1>
+<img :src="Destination.img">
+<h2>{{ clicked }}</h2>
+<button @click="increment">Click me</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const props= defineProps({
+    Destination: Object,
+});
+//clicker stuff
+const clicked=ref(0);
+function increment(){
+    clicked.value++
+}
+</script>
+
+<style scoped>
+
+</style>
