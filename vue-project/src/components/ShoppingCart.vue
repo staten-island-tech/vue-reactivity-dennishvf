f0a4ce523e5ec30f992684f0eaf2a18fadae0561
dennishvf <dennisk38@nycstudents.net>
2024-03-04 09:13:04 -0500

    kdtikd

--- vue-project/src/components/ShoppingCart.vue
+++ vue-project/src/components/ShoppingCart.vue
@@ -4,14 +4,18 @@
 <div v-for="item in props.cart" :key="item.name">
     <h3>{{ item.name }}</h3>
     <h3>{{ item.price }}</h3>
+    <h3>${{ Total() }}</h3>
     </div>
     </div>
 </template>
 
 <script setup>
 
-//export default {};
+
 const props = defineProps({cart:Array})
+const Total =() => {
+return props.cart.reduce((sum, item)=> sum+item.price)
+}
 </script >
 
 <style scoped>
