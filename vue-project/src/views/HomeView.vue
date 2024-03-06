5ffb720c3877b864248cefa2d14fb5d9f21fd7df
dennishvf <dennisk38@nycstudents.net>
2024-03-06 16:45:14 -0500

    blj j

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -20,7 +20,7 @@ import ShoeCard from '@/components/ShoesCard.vue';
 import { ref } from 'vue'
 const shoes= [{    
           name: "Crocs",
-          price: "$40",
+          price: 40,
           img: "https://footwearnews.com/wp-content/uploads/2023/10/Disney-Pixar-Crocs-Classic-Clog-Mater-Release-Date.webp",
         },
         {
